0e606ecda1dd7d0bc76538ead0df350fc78e0408
Akash Sarker <akashsarker2478@gmail.com>
2025-09-02 18:46:23 +0600

    first commit

---
+++ home.js
@@ -0,0 +1,58 @@
+
+const validPin = 1234;
+//home page logout btn
+document.getElementById("log-out-btn" ).
+addEventListener('click', function(e){
+     e.preventDefault()
+    window.location.href = "./login.html";
+})
+//add money button
+document.getElementById("add-money").addEventListener('click', function(e){
+    e.preventDefault()
+    const bank = document.getElementById("bank").value 
+    const accountNumber = document.getElementById("Account-number").value
+    const addAmount = parseInt(document.getElementById("add-amount").value)
+    const pin = parseInt(document.getElementById("pin").value)
+    const avlaiableBalence = parseInt(document.getElementById("Avlaiable-balence").innerText)
+
+    if(accountNumber.length<11){
+        alert('please provide valid account number')
+        return;
+    }
+    if(pin != validPin){
+        alert('please provide valid pin number')
+        return;
+    }
+
+    const newAvlaiableBalence = addAmount + avlaiableBalence;
+    document.getElementById("Avlaiable-balence").innerText = newAvlaiableBalence
+})
+//withdrw button
+document.getElementById("wihdraw-money").addEventListener('click', function(e){
+    e.preventDefault()
+    const agentNumber = document.getElementById("agent-number").value
+    const withdrwAmount = parseInt(document.getElementById("withdrw-ammount" ).value)
+    const pinNumber = parseInt(document.getElementById("pin-number").value)
+    const avlaiableBalenceBefore = parseInt(document.getElementById("Avlaiable-balence").innerText)
+    const totalNewAvlaiableBalence = avlaiableBalenceBefore - withdrwAmount;
+    console.log(totalNewAvlaiableBalence)
+    document.getElementById("Avlaiable-balence").innerText = totalNewAvlaiableBalence
+})
+
+//toggling section
+document.getElementById("add-money-button").addEventListener('click',function(){
+    document.getElementById("cash-out-parent").style.display = 'none'
+    document.getElementById("add-money-parent").style.display = 'block'
+    document.getElementById("transfer-money-parent").style.display = 'none'
+})
+document.getElementById("cash-out-button").addEventListener('click',function(){
+    document.getElementById("add-money-parent").style.display = 'none'
+    document.getElementById("cash-out-parent").style.display = 'block'
+     document.getElementById("transfer-money-parent").style.display = 'none'
+})
+
+document.getElementById("transfer-money-btn").addEventListener('click',function(){
+    document.getElementById("add-money-parent").style.display = 'none'
+    document.getElementById("cash-out-parent").style.display = 'none'
+    document.getElementById("transfer-money-parent").style.display = 'block'
+})
---
+++ script.js
@@ -0,0 +1,17 @@
+//login button
+document.getElementById("login-button").
+addEventListener('click', function(e){
+    e.preventDefault()
+    const mobileNumber = 12345678910;
+    const pin = 1234;
+    const mobileNumberValue = document.getElementById('mobile-number').value ;
+    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+    const pinNumberValue = document.getElementById('pin-number').value;
+    const pinNumberValueConverted = parseInt(pinNumberValue);
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pin){
+        window.location.href = "./home.html"
+    }else{
+        alert('invalid credential')
+    }
+})
+
